aa4ca21ee3cb489b1115c5492860de1de342e59e
James Randall <james@accidentalfish.com>
2016-03-19 19:59:08 +0000

    Initial commit

---
+++ AccidentalFish.ApiUnavailable/Startup.cs
@@ -0,0 +1,20 @@
+﻿using System.Configuration;
+using Microsoft.Owin.Cors;
+using Owin;
+
+namespace AccidentalFish.ApiUnavailable
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            string message = ConfigurationManager.AppSettings["UnavailabilityMessage"];
+            app.UseCors(CorsOptions.AllowAll);
+            app.Run(ctx =>
+            {
+                ctx.Response.StatusCode = 503;
+                return ctx.Response.WriteAsync(message);
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,3 @@
+# Api Unavailable
+
+Simple Owin and ASP.Net based website that will return a 503 Service Unavailable message on any and all requests including those made using the CORS protocol.
